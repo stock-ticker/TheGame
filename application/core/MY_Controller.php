7c86934cf52e019207cb7a02bc0155e091b0e10c
Gaston Beaucage <Gaston.E.Beaucage@gmail.com>
2016-02-16 13:32:46 -0800

    Added MY_CONTROLLER and common_helper

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class Application extends CI_Controller {
+    
+    protected $data = array();
+    protected $id;
+    protected $choices = array('Home' => '/',
+                               'Gallery' => '/gallery',
+                               'About' => '/about');
+    
+    function __construct() {
+        parent::__construct();
+        $this->data = array();
+        $this->data['pagetitle'] = 'Sample Image Gallery';
+        $this->load->library('parser');
+    }
+    
+    function render() {
+        $this->data['menubar'] = build_menu_bar($this->choices);
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+        $this->data['data'] = &$this->data;
+        $this->parser->parse('template', $this->data);
+    }
+}
+
