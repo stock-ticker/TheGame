6a05b9a119097fc7f85f5d68dddc5f5e26824445
dturitsa <dturitsa@gmail.com>
2016-02-16 13:38:54 -0800

    stockHistory view and controller

--- application/controllers/StockHistory.php
+++ application/controllers/StockHistory.php
@@ -6,12 +6,22 @@ class StockHistory extends CI_Controller {
     function __construct()
 	{
 		parent::__construct();
+                
 	}
         
 	public function index()
 	{
-		//$this->data['pagebody'] = 'history';
-               // $this->render();
+            $source = array_map('str_getcsv', file('stocks.csv'));
+            print_r($source);
+            /*
+            foreach ($source as $record)
+		{
+			$stocks[] = array('Code' => $record['Code'], 'Name' => $record['Name'], 'Category' => $record['Category'], 'Value' => $record['Value']);
+		}
+		$this->data['stocks'] = $stocks;
+*/
+            //$this->data['pagebody'] = 'stock_history';
+            //$this->render();
             $this->load->view('stock_history');
 	}
 }
