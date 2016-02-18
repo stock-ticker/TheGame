9a6cf76ad6ae28e22d6c4e1df9d5041951d95521
dturitsa <dturitsa@gmail.com>
2016-02-18 15:08:03 -0800

    all stock History functionality implemente

---
+++ application/views/transaction_history.php
@@ -0,0 +1,16 @@
+     <table>
+        <tr>
+            <th>Player</th>
+            <th>Transaction</th>
+            <th>Quantity</th>
+            <th>DateTime</th>
+         </tr>
+        {transactions}
+            <tr>
+                <td>{Player}</td>
+                <td>{Trans}</td>
+                <td>{Quantity}</td>
+                <td>{DateTime}</td>
+            </tr>
+        {/transactions}
+     </table>
