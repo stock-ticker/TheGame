444fc38768dd52e6a4beffa6d588b86133a0acb2
dturitsa <dturitsa@gmail.com>
2016-04-14 09:10:12 -0700

    made gamestate model with methods to access gamestate info, fixed bug with menubar not appearing on linux

---
+++ application/models/Gamestate.php
@@ -0,0 +1,25 @@
+<?php
+class Gamestate extends CI_Model{
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function getState() {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'http://bsx.jlparry.com/status');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        $response = curl_exec($curl); 
+        curl_close($curl);
+        
+        $xml_resp = new SimpleXMLElement($response);
+        $responseArray = array(
+            'stateDesc' => $xml_resp->desc->__toString(),
+            'stateNum' => $xml_resp->state->__toString(),
+            'countdown' => $xml_resp->state->__toString()
+        );
+        return $responseArray;  
+    }
+}
