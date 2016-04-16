b20f57d8f35f7aa8c61af94400cc420acfee17a6
Gaston Beaucage <Gaston.E.Beaucage@gmail.com>
2016-04-16 10:43:14 -0700

    file upload breaks and I don't know why

--- application/views/registerView.php
+++ application/views/registerView.php
@@ -6,6 +6,7 @@
     Name: <input type="text" name="name" required></input><br/>
     UserID: <input type="text" name="userid" required></input><br/>
     Password: <input type="password" name="password" required></input><br/>
+    <input id="avatar" name="avatar" type="file" value="Upload Avatar">
 <input type="submit" value="Submit"/>
 </form>
 
